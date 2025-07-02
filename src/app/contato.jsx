aafde36aa92ef87e7a1a9fb9144287b34e1bdc53
Karina <karinaalveskap@gmail.com>
2025-07-02 19:54:43 -0300

    feature: criação da página de contato

---
+++ src/app/contato.jsx
@@ -0,0 +1,9 @@
+export default function Contato() {
+  return (
+    <>
+      <div>
+        <h1>Seja bem-vindo à página de Contato</h1>
+      </div>
+    </>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,12 @@
+
 export default function Home() {
   return (
     <>
+      <div>
+        <h1>Prova Versionamento - RPV Senai Cataguases</h1>
+        <p>Karina Alves Pinheiro</p>
+        <p>Cataguases, 02 de Julho de 2025</p>
+      </div>
     </>
-  )
+  );
 }
